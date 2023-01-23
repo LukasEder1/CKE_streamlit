fa527a4395446702f9c2f9ca60201f6204a7344f
LukasEder1 <lukaseder.el@gmail.com>
2023-01-23 19:40:46 +0100

    removed unnecassary comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,8 +136,7 @@ if run:
                                                                             threshold=lower_bound,
                                                                             extra_stopwords=[stopword.lower() for stopword in stopwords],
                                                                             top_k=int(num_splits))
-        #st.write(matched_dicts[0])
-        st.write(ud)
+
         st.markdown("<h1 style='text-align: center;'>Diff-Content and Matched Sentences</h1>", unsafe_allow_html=True)
         st.dataframe(changed_df(added[0], matched_dicts[0], deleted[0]), use_container_width=True)
         st.markdown("<h1 style='text-align: center;'>Contrastive Keywords</h1>", unsafe_allow_html=True)
