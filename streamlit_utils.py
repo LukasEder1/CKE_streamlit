61337df93705af8bfbadc41f21a9ddadcac27303
LukasEder1 <lukaseder.el@gmail.com>
2025-02-22 19:23:54 +0100

    removed annotated_text depency

--- streamlit_utils.py
+++ streamlit_utils.py
@@ -1,13 +1,9 @@
 import pandas as pd
 import streamlit as st
-import nltk
-import numpy as np
 import re
-import pickle
-from annotated_text import annotated_text
 import pysbd
-import math
 from TextHighlight import ContrastiveTextHighlighter
+
 def drop_dupilcates(list):
     result = []
     for i in list:
@@ -53,68 +49,6 @@ def create_keyword_frame(keywords):
         scores.append(score)
     
     return kws, scores
-
-def enumerate_grey_sentences(matched_dict):
-    return {}
-
-def annotate_keywords(documents, intermediate_keywords, changed_indices, matched_dict, new, ngram, added, removed, deleted):
-    seg = pysbd.Segmenter(language="en", clean=False)
-    sentences_a = seg.segment(documents[0])
-    sentences_b = seg.segment(documents[-1])
-    words_a = [re.findall(r"[\w']+", sentences_a[i].lower())  for i in range(len(sentences_a))]
-    words_b = [re.findall(r"[\w']+", sentences_b[i].lower())  for i in range(len(sentences_b))]
-    matched_and_changed = [matched_dict[i][k][0] for i in changed_indices for k in range(len(matched_dict[i]))]
-    keywords = list(intermediate_keywords.keys())
-    for i in changed_indices:
-        for k in range(len(matched_dict[i])):
-            matched_idx, _ = matched_dict[i][k]
-            
-            
-            for j in range(len(words_b[matched_idx])):
-                word = words_b[matched_idx][j]
-
-                if type(word) != str:
-                    break
-
-                if word.lower() in added[i].get(int(matched_idx), []) and word.lower() in keywords:
-                    words_b[matched_idx][j] = (f"{word}", f"{round(intermediate_keywords[word.lower()], 5)}")
-
-                else:
-                    words_b[matched_idx][j] += " "
-
-        for j in range(len(words_a[i])):
-            word = words_a[i][j]
-
-            if type(word) != str:
-                break
-
-            if word.lower() in deleted[i] and word.lower() in keywords:
-                words_a[i][j] = (f"{word}", f"{round(intermediate_keywords[word.lower()], 5)}")
-
-            else:
-                words_a[i][j] += " "
-
-
-    col_former, col_later = st.columns(2)
-    with col_former:
-        st.markdown("<h2 style='text-align: center;'>Original Document</h2>", unsafe_allow_html=True)
-        for i in range(len(words_a)):
-            if i in changed_indices:
-                annotated_text(*words_a[i])
-            elif i in removed:
-                annotated_text((sentences_b[i], "removed", "#ff6666"))
-            else:
-                st.write(sentences_b[i])
-
-    with col_later:
-        st.markdown("<h2 style='text-align: center;'>Latter Document</h2>", unsafe_allow_html=True)
-        for i in range(len(words_b)):
-            if i in matched_and_changed:
-                annotated_text(*words_b[i])
-            elif i in new:
-                annotated_text((sentences_b[i], "new", "#00e600"))
-            else:
-                st.write(sentences_b[i])
 
 def changed_df(added, matched_dict, deleted):
     original_indices = []
@@ -215,85 +149,6 @@ def find_max_indices(matched_dict, matched_indices, changed_indices):
 
 
     return max_index, nonmax_mapping
-
-def highlight_changes(former, later, changed_indices, matched_dict, new, removed, matched_indices):
-
-    # Setup
-
-    # Find the index with the highest Semantic Similarity for all split sentences
-    max_index, nonmax_mapping = find_max_indices(matched_dict, matched_indices, changed_indices)
-    merges, maximum_merges, merges_mapping = find_merges(matched_dict)
-    seg = pysbd.Segmenter(language="en", clean=False)
-    former_sentences = seg.segment(former)
-
-    later_sentences = seg.segment(later)
-
-    # find sentences in newer version that have been matched to
-    # and where the syntatic similarity is below 1.0
-    matched_and_changed = [matched_dict[i][0][0] for i in changed_indices]
-    
-    # includes the split to sentences, without the max split sentence
-    splits = []
-
-    # find non-max split sentences
-    for i, l in matched_dict.items():
-        
-        if len(l) > 1:
-
-            for j, score in l:
-                if int(j) not in list(max_index.values()) and score < 1:
-                    splits.append(int(j))
-                    #split_from.update({str(j): max_index[i]})
-    
-    nonmax_merge = list(merges_mapping.keys())
-
-    # Begin Document
-
-    st.markdown("<h1 style='text-align: center;'>Sentence Matching Results</h1>", unsafe_allow_html=True)
-
-    col_former, col_later = st.columns(2)
-    # Matches for Document A
-
-    # Matches for Document B
-    with col_later:
-        st.markdown("<h2 style='text-align: center;'>Newer Version</h2>", unsafe_allow_html=True)
-        d = {int(i):count for count, i in enumerate(drop_dupilcates(matched_and_changed))}
-
-
-
-        for i in range(len(later_sentences)):
-            if i in new:
-                annotated_text((later_sentences[i], "new", "#00e600"))
-            elif i in splits:
-                annotated_text((later_sentences[i], f"{d[nonmax_mapping[i]]} - split", "#f2f2f2"))
-            elif i in matched_and_changed:
-                annotated_text((later_sentences[i], f"{d[i]}", "#f2f2f2"))
-            else:
-                st.write(later_sentences[i])
-
-
-    with col_former:
-        st.markdown("<h2 style='text-align: center;'>Older Version</h2>", unsafe_allow_html=True)
-
-        # use the annotated component to highlight text
-        for i in range(len(former_sentences)):
-            if i in removed:
-                annotated_text((former_sentences[i], "removed", "#ff6666"))
-            elif i in changed_indices:
-                """
-                if not i in nonmax_merge:
-                    annotated_text((former_sentences[i], f"{matched_dict[i][0][0]}", "#f2f2f2"))
-                else:
-                    annotated_text((former_sentences[i], f"{matched_dict[i][0][0]} - merge", "#f2f2f2"))
-                """
-                annotated_text((former_sentences[i], f"{d[int(matched_dict[i][0][0])]}", "#f2f2f2"))
-
-
-                
-            else:
-                st.write(former_sentences[i])
-
-
 
 def show_sentence_importances(ranking, former, later):
 
